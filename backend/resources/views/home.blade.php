e8aa47b556a2f1cd1555e7c3032712294a68c5b7
shintarou <thirdman_shin@yahoo.co.jp>
2021-03-23 00:12:21 +0900

    3/23

---
+++ backend/resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title','シューカツ')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/home.css') }}">
+@endsection
+
+@section('content')
+<div>
+    <p>aaaaaaaaaaaaa</p>
+</div>
+
+@endsection
